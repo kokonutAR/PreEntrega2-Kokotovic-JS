264e5c6fc97c1de228852f8c9d9234d708f082bc
kokonutAR <juan.kokotovic@gmail.com>
2023-02-13 16:40:50 -0300

    objetos

--- Js/main.js
+++ Js/main.js
@@ -30,5 +30,44 @@ if(cantPasajeros <= 13){
 let precioPasajero = (a,cantPasajeros) => {
     return a / cantPasajeros;
 }
-alert("El precio del viaje, por pasajero, sera: " + precioPasajero(a,cantPasajeros) + " Pesos Argentinos")
-console.log(precioPasajero(a,cantPasajeros) + " Pesos Argentinos por pasajero.")
+
+alert("El precio del viaje, por pasajero, sera: " + precioPasajero(a,cantPasajeros) + " Pesos Argentinos");
+console.log(precioPasajero(a,cantPasajeros) + " Pesos Argentinos por pasajero.");
+
+
+class Viaje{
+    constructor(nombre, precio){
+        this.nombre = nombre;
+        this.precio = precio;   
+    }
+}
+
+const combi = new Viaje("Combi", 12000);
+const minibus = new Viaje("Minibus", 16000);
+
+const arrayViajes = [combi, minibus];
+
+const combiLargaDistancia = new Viaje("Combi Larga Distancia", 32000);
+const minibusLargaDistancia = new Viaje("Minibus Larga Distancia", 40000);
+
+arrayViajes.push(combiLargaDistancia, minibusLargaDistancia);
+
+console.log(arrayViajes);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
